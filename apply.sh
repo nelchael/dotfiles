73e6efad74158913fbc5537c92e8c888702ef41c
Krzysztof Pawlik (nelchael) <none@none>
2010-10-01 14:01:18 +0200

    Use list of files.

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for i in *; do
-	[[ "${i}" == "apply.sh" ]] && continue
+APPLY_FILES="bashrc hgignore hgrc vimrc"
+
+for i in ${APPLY_FILES}; do
 	colordiff -u "${HOME}/.${i}" "${i}"
 	cp -fv "${i}" "${HOME}/.${i}"
 done
