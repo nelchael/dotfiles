861c543c1e129d0dee502465196cac6f49140d27
Krzysztof Pawlik <none@none>
2012-05-30 20:01:38 +0200

    Add inputrc: readline settings & key bindings.

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APPLY_FILES="bashrc hgignore hgrc screenrc vimrc"
+APPLY_FILES="bashrc hgignore hgrc inputrc screenrc vimrc"
 
 for i in ${APPLY_FILES}; do
 	colordiff -u "${HOME}/.${i}" "${i}"
