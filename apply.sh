ae672f2889b01f5a211b49d6747902192c67892a
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2018-12-04 14:16:21 +0100

    Add support for maintaining directories.

--- apply.sh
+++ apply.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 APPLY_FILES="bashrc dir_colors gitconfig inputrc screenrc vimrc"
+APPLY_DIRECTORIES="vim-autoload:.vim/autoload vim-colors:.vim/colors vim-plugin:.vim/plugin"
 
 __DIFF="$(type -P colordiff)"
 [[ -z "${__DIFF}" ]] && __DIFF="diff"
 
 for i in ${APPLY_FILES}; do
-	"${__DIFF}" -u "${HOME}/.${i}" "${i}"
-	cp -fv "${i}" "${HOME}/.${i}"
+	[[ -z "$(diff -Nu "${i}" "${HOME}/.${i}")" ]] && continue
+
+	"${__DIFF}" -Nu "${HOME}/.${i}" "${i}"
+	\cp -f "${i}" "${HOME}/.${i}"
+done
+
+echo "${APPLY_DIRECTORIES} " | while IFS=":" read -d " " -s src_dir dest_dir; do
+	[[ -z "${src_dir}" || -z "${dest_dir}" ]] && continue
+	dest_dir="${HOME}/${dest_dir}"
+	[[ -z "$(diff -Nru "${src_dir}" "${dest_dir}")" ]] && continue
+
+	"${__DIFF}" -Nru "${src_dir}" "${dest_dir}"
+	rsync --archive --human-readable --delete "${src_dir}"/* "${dest_dir}"
 done
