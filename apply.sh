f9a21c9241d471db971e6b71b01ef14b49e04d77
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2019-03-08 20:45:13 +0100

    Fix diff for directories.

--- apply.sh
+++ apply.sh
@@ -18,7 +18,7 @@ echo "${APPLY_DIRECTORIES} " | while IFS=":" read -d " " -s src_dir dest_dir; do
 	dest_dir="${HOME}/${dest_dir}"
 	[[ -z "$(diff -Nru "${src_dir}" "${dest_dir}")" ]] && continue
 
-	"${__DIFF}" -Nru "${src_dir}" "${dest_dir}"
+	"${__DIFF}" -Nru "${dest_dir}" "${src_dir}"
 	rm -rf "${dest_dir}"
 	mkdir -p "${dest_dir}"
 	\cp -fRv "${src_dir}"/* "${dest_dir}/"
