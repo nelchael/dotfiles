dffc096a8358b33c3e975fa3b8eccbe173c3ce02
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2020-09-15 17:45:33 +0200

    Remove vim stuff.

---
+++ README.md
@@ -0,0 +1,24 @@
+# dotfiles
+
+## Applying
+
+Run `./apply.sh`.
+
+**Warning**: overwrites without prompting (but shows a diff).
+
+## vim plugins
+
+In `.vim/pack/plugins/start` clone:
+
+* <https://github.com/cespare/vim-toml>
+* <https://github.com/ciaranm/inkpot/>
+* <https://github.com/ciaranm/securemodelines>
+* <https://github.com/hashivim/vim-terraform>
+* <https://github.com/itchyny/lightline.vim>
+* <https://github.com/itchyny/vim-gitbranch>
+* <https://github.com/mustache/vim-mustache-handlebars>
+* <https://github.com/natw/keyboard_cat.vim>
+
+## userContent.css
+
+Tweaks Firefox new and blank tab background colors, put in `.mozilla/firefox/<PROFILE ID>.default/chrome/`.
--- apply.sh
+++ apply.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
 APPLY_FILES="bashrc dir_colors gitconfig inputrc screenrc vimrc"
-APPLY_DIRECTORIES="vim-autoload:.vim/autoload vim-colors:.vim/colors vim-plugin:.vim/plugin vim-syntax:.vim/syntax vim-indent:.vim/indent"
 
 for i in ${APPLY_FILES}; do
 	[[ -z "$(diff -Nu "${i}" "${HOME}/.${i}")" ]] && continue
 
 	diff --color=auto -Nu "${HOME}/.${i}" "${i}"
 	\cp -f "${i}" "${HOME}/.${i}"
-done
-
-echo "${APPLY_DIRECTORIES} " | while IFS=":" read -d " " -s src_dir dest_dir; do
-	[[ -z "${src_dir}" || -z "${dest_dir}" ]] && continue
-	dest_dir="${HOME}/${dest_dir}"
-	[[ -z "$(diff -Nru "${src_dir}" "${dest_dir}")" ]] && continue
-
-	diff --color=auto -Nru "${dest_dir}" "${src_dir}"
-	rm -rf "${dest_dir}"
-	mkdir -p "${dest_dir}"
-	\cp -fRv "${src_dir}"/* "${dest_dir}/"
 done
