d23cbd643641bed9ba6837750620781a6cf31fc7
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2021-10-04 19:48:19 +0200

    Improve script for offline usage, drop verbose mode.

--- misc-scripts/sync-all.sh
+++ misc-scripts/sync-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-options="$(getopt -o opbfv -- "${@}")"
+options="$(getopt -o opbf -- "${@}")"
 if [[ "${?}" -ne 0 ]]; then
 	exit 1
 fi
@@ -10,17 +10,14 @@ while :; do
 		-o)
 			declare -r option_offline=yes
 			;;
-		-p)
-			declare -r option_force_gcp=yes
+		-f)
+			declare -r option_run_gc_pruge_all=yes
 			;;
 		-b)
 			declare -r option_hide_branches=yes
 			;;
-		-f)
-			declare -r option_force_purge=yes
-			;;
-		-v)
-			declare -r option_verbose=yes
+		-p)
+			declare -r option_purge_branches=yes
 			;;
 		--)
 			shift
@@ -45,7 +42,6 @@ GIT_OPTIONS="-c core.useBuiltinFSMonitor=false"
 
 for directory in *; do
 	[[ -d "${directory}/.git" ]] || {
-		[[ "${option_verbose}" = "yes" ]] && echo -e " \e[1;91m❯\e[0m \e[1m${directory}\e[0m - \e[31mskipped\e[0m";
 		continue;
 	}
 
@@ -58,34 +54,34 @@ for directory in *; do
 		else
 			branch_info="\e[2m@\e[0m\e[92m${branch_info}\e[0m"
 		fi
-		ahead="$(git ${GIT_OPTIONS} -C "${directory}" rev-list @{upstream}..HEAD --count 2> /dev/null)"
-		behind="$(git ${GIT_OPTIONS} -C "${directory}" rev-list HEAD..@{upstream} --count 2> /dev/null)"
-		[[ "${ahead}" != "" && "${ahead}" != "0" ]] && branch_info="${branch_info}\e[96m+${ahead}\e[0m"
-		[[ "${behind}" != "" && "${behind}" != "0" ]] && branch_info="${branch_info}\e[96m-${behind}\e[0m"
+		[[ "${option_offline}" = "yes" ]] || {
+			ahead="$(git ${GIT_OPTIONS} -C "${directory}" rev-list @{upstream}..HEAD --count 2> /dev/null)"
+			behind="$(git ${GIT_OPTIONS} -C "${directory}" rev-list HEAD..@{upstream} --count 2> /dev/null)"
+			[[ "${ahead}" != "" && "${ahead}" != "0" ]] && branch_info="${branch_info}\e[96m+${ahead}\e[0m"
+			[[ "${behind}" != "" && "${behind}" != "0" ]] && branch_info="${branch_info}\e[96m-${behind}\e[0m"
+		}
 	fi
 	echo -e " \e[1;93m❯\e[0m \e[1m${directory}\e[0m${branch_info} \e[2m$(git ${GIT_OPTIONS} -C "${directory}" remote get-url origin 2> /dev/null || echo 'no remote origin')\e[0m"
 
-	if [[ -n "$(git ${GIT_OPTIONS} -C "${directory}" remote show)" ]]; then
-		[[ "${option_offline}" = "yes" ]] || {
-			(git ${GIT_OPTIONS} -C "${directory}" pull --rebase || git ${GIT_OPTIONS} -C "${directory}" status) 2>&1 | "${SED}" -re "/^Current branch .+ is up to date/d; /^Already up to date./d; /^Fetching submodule/d; ${SYNC_ALL_EXTRA_SED};"
+	if [[ "${option_offline}" != "yes" && -n "$(git ${GIT_OPTIONS} -C "${directory}" remote show)" ]]; then
+		(git ${GIT_OPTIONS} -C "${directory}" pull --rebase || git ${GIT_OPTIONS} -C "${directory}" status) 2>&1 | "${SED}" -re "/^Current branch .+ is up to date/d; /^Already up to date./d; /^Fetching submodule/d; ${SYNC_ALL_EXTRA_SED};"
 
-			echo -ne "\e[31m"
-			git ${GIT_OPTIONS} -C "${directory}" branches | grep '\[gone\]'
-			echo -ne "\e[0m"
-		}
+		echo -ne "\e[31m"
+		git ${GIT_OPTIONS} -C "${directory}" branches | grep '\[gone\]'
+		echo -ne "\e[0m"
 
-		[[ "${option_force_purge}" = "yes" ]] && {
+		[[ "${option_purge_branches}" = "yes" ]] && {
 			git ${GIT_OPTIONS} -C "${directory}" purge-branches
 		}
-	else
-		[[ "${option_verbose}" = "yes" ]] && echo -e " \e[1;91m❯\e[0m \e[1m${directory}\e[0m - \e[31mno remotes, skippied pull\e[0m";
+
+		if [[ "${option_run_gc_pruge_all}" != "yes" ]]; then
+			git ${GIT_OPTIONS} -C "${directory}" gc --auto --quiet
+			git ${GIT_OPTIONS} -C "${directory}" submodule --quiet foreach 'git ${GIT_OPTIONS} gc --auto --quiet'
+		fi
 	fi
 
-	if [[ "${option_force_gcp}" = "yes" ]]; then
+	if [[ "${option_run_gc_pruge_all}" = "yes" ]]; then
 		git ${GIT_OPTIONS} -C "${directory}" gc --prune=all --quiet
 		git ${GIT_OPTIONS} -C "${directory}" submodule --quiet foreach 'git ${GIT_OPTIONS} gc --prune=all --quiet'
-	else
-		git ${GIT_OPTIONS} -C "${directory}" gc --auto --quiet
-		git ${GIT_OPTIONS} -C "${directory}" submodule --quiet foreach 'git ${GIT_OPTIONS} gc --auto --quiet'
 	fi
 done
