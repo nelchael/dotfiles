ce52bb9c3582bb65049696f27b3489278e8e10ba
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2023-11-24 18:38:49 +0100

    Check for deprecated files.

--- apply.sh
+++ apply.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 APPLY_FILES="bashrc gitconfig ripgreprc vimrc"
+DEPRECATED_FILES="dir_colors inputrc screenrc"
 
 for i in ${APPLY_FILES}; do
 	[[ -z "$(diff -Nu "${i}" "${HOME}/.${i}")" ]] && continue
@@ -8,3 +9,9 @@ for i in ${APPLY_FILES}; do
 	diff --color=auto -Nu "${HOME}/.${i}" "${i}"
 	\cp -f "${i}" "${HOME}/.${i}"
 done
+
+for i in ${DEPRECATED_FILES}; do
+	[[ -e "${HOME}/.${i}" ]] && { echo " >>> Warning: ~/.${i} exists"; }
+done
+
+true
