516d92cad8242bcc985ad90ac67d7b9f1e74bd44
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2024-03-18 22:47:58 +0100

    Tune completion display.

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-APPLY_FILES="bashrc gitconfig ripgreprc vimrc"
-DEPRECATED_FILES="dir_colors inputrc screenrc"
+APPLY_FILES="bashrc gitconfig inputrc ripgreprc vimrc"
+DEPRECATED_FILES="dir_colors screenrc"
 
 for i in ${APPLY_FILES}; do
 	[[ -z "$(diff -Nu "${i}" "${HOME}/.${i}")" ]] && continue
