eb3d512db15930a0e55686c9a674986108c0d9c9
Krzysztof Pawlik <krzysiek.pawlik@people.pl>
2024-03-23 14:15:54 +0100

    Simplify script.

--- misc-scripts/docker-refresh-images.sh
+++ misc-scripts/docker-refresh-images.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-SED="$(which sed)"
-"${SED}" --version &> /dev/null || SED="$(which gsed)"
-"${SED}" --version &> /dev/null || {
-    echo "Failed to find working GNU sed"
-    exit 1
-}
-
-for img in $(docker image ls | "${SED}" 1d | sort | awk '{ print $1 ":" $2 }'); do
+for img in $(docker image ls --format=json | jq -r '.Repository + ":" + .Tag' | tr -d '\r'); do
     [[ "${img}" = *":<none>" ]] && continue
-    repo_digest="$(docker inspect "${img}" | jq -r '.[].RepoDigests[0]')"
+    repo_digest="$(docker image inspect "${img}" | jq -r '.[].RepoDigests[0]')"
     if [[ "${repo_digest}" != "null" ]]; then
         echo -e " \e[92m>>>\e[0m Pulling \e[94m${img}\e[0m"
         docker image pull "${img}"
